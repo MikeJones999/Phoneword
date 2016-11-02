c8bceb60cf29de272a1fac1827063a7028a943e6
MIKIEJ-DESKTOP\mikiej <mikiej_boy@hotmail.com>
2016-11-02 18:39:43 +0000

    Added initial MainPage and initial layout of page

---
+++ Phoneword/Phoneword/MainPage.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Phoneword
+{
+    class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+
+            this.Padding = new Thickness(20,
+            Device.OnPlatform<double>(40, 20, 20), 20, 20);
+
+
+            Button button1 = new Button
+            {
+                Text = "Translate",
+                Font = Font.SystemFontOfSize(NamedSize.Large),
+                BorderWidth = 1,
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.CenterAndExpand
+            };
+            button1.Clicked += OnButtonClicked;
+
+
+            Button button2 = new Button
+            {
+                Text = "Call",
+                Font = Font.SystemFontOfSize(NamedSize.Large),
+                BorderWidth = 1,
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.CenterAndExpand,
+                IsEnabled = false
+            };
+            button2.Clicked += OnButtonClicked;
+
+
+            Label label = new Label
+            {
+                Text = "Enter a Phoneword",
+                Font = Font.SystemFontOfSize(NamedSize.Large),
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.CenterAndExpand
+            };
+
+            Entry entry = new Entry
+            {
+                Placeholder = "e.g. 1-855-XAMARIN"
+            };
+
+            Content = new StackLayout
+            {
+                VerticalOptions = LayoutOptions.FillAndExpand,
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+                Orientation = StackOrientation.Vertical,
+                Spacing = 15,
+
+                Children =
+                {
+                    label,
+                    entry,             
+                    button1,
+                    button2
+                    
+                }
+            };
+
+
+         
+        }
+
+        private void OnButtonClicked(object sender, EventArgs e)
+        {
+           //PhonewordTranslator.ToNumber()
+        }
+    }
+}
---
+++ Phoneword/Phoneword/PhonewordTranslator.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phoneword
+{
+    public static class PhonewordTranslator
+    {
+        public static string ToNumber(string raw)
+        {
+            if (string.IsNullOrWhiteSpace(raw))
+                return null;
+
+            raw = raw.ToUpperInvariant();
+
+            var newNumber = new StringBuilder();
+            foreach(var chr in raw)
+            {
+                if (" -0123456789".Contains(chr))
+                {
+                    newNumber.Append(chr);
+                }
+                else
+                {
+                    var result = TranslateToNumber(chr);
+                    if(result != null)
+                    {
+                        newNumber.Append(result);
+                    }
+                    else
+                    {
+                        //possible bad character
+                        return null;
+                    }
+                }
+            }
+            return newNumber.ToString();
+        }
+
+
+        static readonly string[] digits = {
+            "ABC", "DEF", "GHI", "JKL", "MNO", "PQRS", "TUV", "WXYZ"
+        };
+
+        private static object TranslateToNumber(char c)
+        {
+            for (int i = 0; i < digits.Length; i++)
+            {
+                if (digits[i].Contains(c))
+                {
+                    return 2 + i;
+                }
+             
+            }
+
+            return null;
+        }
+
+        static bool Contains(this string keyString, char c)
+        {
+            return keyString.IndexOf(c) >= 0;
+        }
+    }
+}
